1d9f52f9d8488c11c9543d4b5f87ae03da5ce0db
WhiteBorshek <121310443+WhiteBorshek@users.noreply.github.com>
2023-04-17 21:34:05 +0500

    Add files via upload

---
+++ HometaskI.java
@@ -0,0 +1,21 @@
+package Hometasks;
+
+import java.util.Scanner;
+
+public class HometaskI {
+    public static void main(String[] args) {
+        Scanner newScanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = newScanner.nextInt();
+        newScanner.close();
+        System.out.println("Треугольное число: " + Triangle(a) + "\n");
+    }
+
+    public static int Triangle(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return Triangle(num - 1) + num;
+    }
+}
+
